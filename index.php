919281a1e3d7a4802318169a15952283a75d1655
niels gebben <nielsgebben@outlook.com>
2017-06-14 12:41:42 +0200

    landing page

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="col-sm-12" style="background-color: #3366FF"><p class="text-white"><h2><center> Sector Page </center></h2></p></div>
+
+            <div class="row row-centered">
+                <div class="col-xs-3 col-centered" style="height: 13%"></div>
+            </div>
+            <div class="row row-centered">
+
+                <div class="col-xs-4 col-centered" >
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%; padding: 8% 0;  "><center><h2>Applicatie Ontwikkelaar</h2></center></div>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-4 col-centered">
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%; padding: 8% 0;  "><center><h2>ICT Helpdesk</h2></center></div>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-4 col-centered">
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%; padding: 8% 0;  "><center><h2>Netwerk Beheer</h2></center></div>
+                       
+                    </div>
+                </div>
+            </div>
+
+            <div class="row row-centered">
+
+                <div class="col-xs-4 col-centered">
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%;  padding: 7% 0;  "><center><h2>Docent Overzicht</h2></center></div>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-4 col-centered">
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%; padding: 7% 0;  "><center><h2>Loopbaan Begeleiding</h2></center></div>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-4 col-centered">
+                    <div class="panel panel-default" style="background-color: #8da9fc">
+                        <div class="panel-body" style="height: 20%; padding: 7% 0;  "><center><h2>Rooster</h2></center></div>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+    </body>
+</html>
