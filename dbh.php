d20838ddef1d18e17f1f5647950012312f062467
DESKTOP-B87CD5K\Paco-pc <pablo_nicolaas@live.nl>
2017-06-21 12:52:41 +0200

    bettere fail checks op conectie maken

--- dbh.php
+++ dbh.php
@@ -1,7 +1,18 @@
 <?php
 
-$conn = mysqli_connect ("localhost", "root", "","213server");
+$sHost = 'localhost';
+$sUser = 'root';
+$sPass = '';
+$sDB = '213server';
+//create connection
+$conStr = mysqli_connect($sHost, $sUser, $sPass, $sDB);
 
-if (!$conn) {
-    die("onnection failed: ".mysqli_connect_error());
+// check connection
+if (!($conStr)) {
+    die('Failed to connect to MySQL Database Server - #' . mysqli_connect_errno() . ': ' . mysqli_Connect_error());
+    if (!mysqli_select_db('slb')) {
+        die('Connected to Server, but Failed to Connect to Database - #' . mysqli_connect_errno() . ': ' . mysqli_connect_errno());
+    }
+} else {
+
 }
