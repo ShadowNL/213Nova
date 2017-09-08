dc124a8f4f042e9232b9ffc76c57fe0be3d5d5db
niels gebben <nielsgebben@outlook.com>
2017-09-08 12:48:09 +0200

    helptext

--- index.php
+++ index.php
@@ -26,18 +26,18 @@ if ($result && $result->num_rows > 0) {
     //admin add vak
     if (isset($_SESSION['username'])) {
         echo '<div class="opdrachten-label" style="height: 30px;">
- <div id="addOpdrachtBtn" type="button" class="opdrachten-label-header" data-toggle="modal" data-target="#AdminAddOpdrachtModal" VakID="'.$VakID.'" style="text-align: center; line-height: 20px;">
+ <div id="addOpdrachtBtn" type="button" class="opdrachten-label-header" data-toggle="modal" data-target="#AdminAddOpdrachtModal" VakID="' . $VakID . '" style="text-align: center; line-height: 20px;">
  <a href="#" class="btn-addopdracht">triggert</a>
  </div>
 </div>';
     }
 
     while ($row = $result->fetch_assoc()) {
-        echo    "<div class='opdrachten-label'>
+        echo "<div class='opdrachten-label'>
                     <div class='opdrachten-label-header'><b>" . $row["Titel"] . "</b></div>
                     <div class='opdrachten-label-textbox'>" . $row["Omschrijving"] . "</div>
                     <div class='opdrachten-label-teacher'> leraar: " . $row["Verantwoordelijke"] .
-                        "<div class='opdrachten-label-download-btn'>
+        "<div class='opdrachten-label-download-btn'>
                             <a href=" . $row["Downloadlink"] . " download>download</a>
                         </div>
                         <div class='opdrachten-label-download-btn' data-toggle=\"modal\" data-target=\"#adminLoginModal\">
@@ -48,16 +48,30 @@ if ($result && $result->num_rows > 0) {
     }
 } else {
     if ($VakID != 0) {
-        echo "Geen opdrachten in dit vak gevonden";
+        ?>
+        <div class="opdrachten-label" style="height: 30px;">
+            <div class="opdrachten-label-header"   style="text-align: center; line-height: 20px;">
+                <div class=btn-addopdracht>Geen opdrachten gevonden in dit vak
+                </div>
+            </div>
+        </div>
+        <?php
         if (isset($_SESSION['username'])) {
             echo '<div class="opdrachten-label" style="height: 30px;">
- <div id="addOpdrachtBtn" type="button" class="opdrachten-label-header" data-toggle="modal" data-target="#AdminAddOpdrachtModal" VakID="'.$VakID.'" style="text-align: center; line-height: 20px;">
+ <div id="addOpdrachtBtn" type="button" class="opdrachten-label-header" data-toggle="modal" data-target="#AdminAddOpdrachtModal" VakID="' . $VakID . '" style="text-align: center; line-height: 20px;">
  <a href="#" class="btn-addopdracht">triggert</a>
  </div>
 </div>';
         }
+    } else {
+            ?>  
+            <div class="opdrachten-label" style="height: 70px;">
+                <div class="opdrachten-label-header"   style="height: 70px; text-align: center; line-height: 20px;">
+                    <div class=opdrachten-label-header style="text-align: center"><h4>Om opdrachten te kunnen downloaden en of bekijken<br>moet u eerst nog uw vak kiezen</h4>
+                    </div>
+                </div>
+            </div>
+            <?php
     }
 }
-
-
 ?>
