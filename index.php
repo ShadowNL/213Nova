a077e9a9e1f8da1dcc320885bc89c4f3b3b756e8
Wessel Bos <wescar245@gmail.com>
2017-09-13 13:12:20 +0200

    functies gemaakt voor het genereren van buttons en berichten

--- index.php
+++ index.php
@@ -15,66 +15,77 @@ global $SectorID;
 global $VakID;
 $sqlNav = "SELECT * FROM opdrachten WHERE VakID = " . $VakID;
 $result = $conStr->query($sqlNav);
-
 $userSetSession = isset($_SESSION['username']);
+
+//Produceer AddOpdracht-knop
+function genAddOpdrachtButton($VakID){
+    echo    '<div class="opdrachten-label" style="height: 30px;">
+                <div id="addOpdrachtBtn" type="button" class="btn-addopdracht" data-toggle="modal" 
+                 data-target="#AdminAddOpdrachtModal" VakID="' . $VakID . '">
+                    Nieuwe Opdracht
+                </div>
+            </div>';
+}
+//Produceer "geen opdrachten" bericht
+function genGeenOpdrachtenLabel(){
+    echo    '<div class="opdrachten-label" style="height: 30px;">
+                <div class="opdrachten-label-header"   style="text-align: center; line-height: 20px;">
+                    <div class=btn-addopdracht>Geen opdrachten gevonden in dit vak
+                    </div>
+                </div>
+            </div>';
+}
+//Produceer Help text
+function genHelpTextLabel(){
+    echo    '<div class="opdrachten-label" style="height: 70px;">
+                <div class="opdrachten-label-header"   style="height: 70px; text-align: center; line-height: 20px;">
+                    <div class=opdrachten-label-header style="text-align: center"><h4>Om opdrachten te kunnen downloaden en of bekijken<br>moet u eerst nog uw vak kiezen</h4>
+                    </div>
+                </div>
+            </div>';
+}
+//Produceer opdrachten
+function genOpdrachtLabel($titel,$omschrijving,$verantwoordelijke,$downloadlink,$userSetSession){
+    echo    "<div class='opdrachten-label'>
+                <div class='opdrachten-label-header'><b>" . $titel . "</b></div>
+                <div class='opdrachten-label-textbox'>" . $omschrijving . "</div>
+                <div class='opdrachten-label-teacher'> leraar: " . $verantwoordelijke .
+                    "<div class='opdrachten-label-download-btn'>
+                        <a href=" . $downloadlink . " download>download</a>
+                    </div>";
+    //Als user ingelogd is, laat EDIT knop zien
+    if ($userSetSession) {
+        echo        "<div class='opdrachten-label-download-btn' data-toggle=\"modal\" data-target=\"#adminLoginModal\">
+                        <a href=\"#\">Edit</a>
+                    </div>";
+    }
+    echo        "</div>
+            </div>";
+}
+
 //Als er opdrachten in het huidige vak staan:
 if ($result && $result->num_rows > 0) {
     //Als user ingelogd is, voeg knop toe, AddOpdracht
     if ($userSetSession) {
-        //Produceer AddOpdracht-knop
-        echo    '<div class="opdrachten-label" style="height: 30px;">
-                    <div id="addOpdrachtBtn" type="button" class="btn-addopdracht" data-toggle="modal" 
-                     data-target="#AdminAddOpdrachtModal" VakID="' . $VakID . '">
-                        Nieuwe Opdracht
-                    </div>
-                </div>';
+        genAddOpdrachtButton($VakID);
     }
     //Genereer html voor alle opdrachten in het huidige vak
     while ($row = $result->fetch_assoc()) {
         //produceer opdracht-labels
-        echo    "<div class='opdrachten-label'>
-                    <div class='opdrachten-label-header'><b>" . $row["Titel"] . "</b></div>
-                    <div class='opdrachten-label-textbox'>" . $row["Omschrijving"] . "</div>
-                    <div class='opdrachten-label-teacher'> leraar: " . $row["Verantwoordelijke"] .
-                        "<div class='opdrachten-label-download-btn'>
-                            <a href=" . $row["Downloadlink"] . " download>download</a>
-                        </div>";
-        //Als user ingelogd is, laat EDIT knop zien
-        if ($userSetSession) {
-            echo        "<div class='opdrachten-label-download-btn' data-toggle=\"modal\" data-target=\"#adminLoginModal\">
-                            <a href=\"#\">Edit</a>
-                        </div>";
-        }
-        echo        "</div>
-                </div>";
+        genOpdrachtLabel($row["Titel"],$row["Omschrijving"],$row["Verantwoordelijke"],$row["Downloadlink"],$userSetSession);
     }
 //Als er geen opdrachten gevonden zijn:
 } else {
     //als er een vak is geselecteerd, geef bericht dat er geen opdrachten zijn
     if ($VakID != 0) {
-        echo    '<div class="opdrachten-label" style="height: 30px;">
-                    <div class="opdrachten-label-header"   style="text-align: center; line-height: 20px;">
-                        <div class=btn-addopdracht>Geen opdrachten gevonden in dit vak
-                        </div>
-                    </div>
-                </div>';
+        genGeenOpdrachtenLabel();
         //Als user is ingelogd, geef ook add-opdrachtknop weer
         if ($userSetSession) {
-            echo    '<div class="opdrachten-label" style="height: 30px;">
-                        <div id="addOpdrachtBtn" type="button" class="opdrachten-label-header" data-toggle="modal" 
-                         data-target="#AdminAddOpdrachtModal" VakID="' . $VakID . '" style="text-align: center; line-height: 20px;">
-                            <a href="#" class="btn-addopdracht">triggert</a>
-                        </div>
-                    </div>';
+            genAddOpdrachtButton($VakID);
         }
     //Als er geen vak geselecteerd is, geef helptext weer
     } else {
-        echo    '<div class="opdrachten-label" style="height: 70px;">
-                    <div class="opdrachten-label-header"   style="height: 70px; text-align: center; line-height: 20px;">
-                        <div class=opdrachten-label-header style="text-align: center"><h4>Om opdrachten te kunnen downloaden en of bekijken<br>moet u eerst nog uw vak kiezen</h4>
-                        </div>
-                    </div>
-                </div>';
+        genHelpTextLabel();
     }
 }
 ?>
